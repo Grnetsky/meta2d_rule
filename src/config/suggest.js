508fffd80a6cbe016a982f06f6c402bf9317d13e
Grnetsky <1101767893@qq.com>
2024-01-26 14:22:22 +0800

    feat:抛出异常进行逻辑整理

--- src/config/suggest.js
+++ src/config/suggest.js
@@ -1,10 +1,11 @@
 const ErrorSuggest = {
     'MaximumCallStackSizeExceeded':'似乎某个地方有死循环了，快检查一下',
-    'InvalidOrUnexpectedToken': '代码书写错啦，检测一下吧'
+    'InvalidOrUnexpectedToken': '代码书写错啦，检测一下吧',
+    'Default': '似乎哪里出错了，再检查一下吧'
 }
 
 export function getErrorSuggest(message) {
-    return ErrorSuggest[camelize(message)]
+    return ErrorSuggest[camelize(message)] || ErrorSuggest.Default
 }
 
 function camelize(str) {
